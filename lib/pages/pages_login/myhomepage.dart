839c44f946fdc1704d46577533a741bc428ed763
DevRollins <drollins1108@gmail.com>
2024-02-15 16:20:59 -0500

    Can move between home and profile now

--- lib/pages/pages_login/myhomepage.dart
+++ lib/pages/pages_login/myhomepage.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:relay/colors/colors.dart';
+import 'package:relay/pages/profile.dart';
 
-class MyHomePage extends StatefulWidget { // changed this to stateful widget
+class MyHomePage extends StatefulWidget {
+  // changed this to stateful widget
   MyHomePage({super.key});
 
   @override
@@ -14,9 +16,9 @@ class _MyHomePageState extends State<MyHomePage> {
   final user = FirebaseAuth.instance.currentUser!;
   //int currentIndex = 0; // navigation
 
-   // sign user out
-  void signUserOut(){
-     FirebaseAuth.instance.signOut();
+  // sign user out
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
   }
 
   @override
@@ -26,52 +28,52 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [
           IconButton(
             onPressed: signUserOut,
-             icon: Icon(Icons.logout),
-             )
-         ],
-        ),
-      body: Center(child: Text(
-        "LOGGED IN AS: " + user.email!,
-        style: TextStyle(fontSize: 20),
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "LOGGED IN AS: " + user.email!,
+          style: TextStyle(fontSize: 20),
         ),
       ),
-
 
       // navbar stuff
       bottomNavigationBar: Container(
         //currentIndex: currentIndex, // navagation btwn pages
-        //onTap: (index) => setState(() => currentIndex = index), 
+        //onTap: (index) => setState(() => currentIndex = index),
 
         color: AppColors.lightBrown,
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
           child: GNav(
-            backgroundColor: AppColors.lightBrown,
-            color: AppColors.grayBlue,
-            activeColor: AppColors.cloudBlue,
-            tabBackgroundColor: AppColors.darkBrown,
-            gap: 8,
-           
-            padding: EdgeInsets.all(10),
-            tabs: [
-              GButton(
-                icon: Icons.home,
-                text: "Home"
-              ),// chat chat_bubble
-              GButton(
-                icon: Icons.add_comment,
-                text: "Create Chat",
-              ), // add_circle 
-              GButton(
-                icon: Icons.person,
-                text: "Profile",
-              ),
-            ]
-          ),
+              backgroundColor: AppColors.lightBrown,
+              color: AppColors.grayBlue,
+              activeColor: AppColors.cloudBlue,
+              tabBackgroundColor: AppColors.darkBrown,
+              gap: 8,
+              padding: const EdgeInsets.all(10),
+              tabs: [
+                const GButton(icon: Icons.home, text: "Home"), // chat chat_bubble
+                const GButton(
+                  icon: Icons.add_comment,
+                  text: "Create Chat",
+                ), // add_circle
+                GButton(
+                  icon: Icons.person,
+                  text: "Profile",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfilePage()),
+                    );
+                  },
+                ),
+              ]),
         ),
       ),
-
-
     );
   }
 }
