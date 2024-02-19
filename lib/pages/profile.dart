519d0b9099c9059fe16124d35ba80b6dbca36e46
DevRollins <drollins1108@gmail.com>
2024-02-19 12:51:18 -0500

    Added back button and made it look prettier

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:relay/components/navbar.dart';
+import 'package:relay/pages/pages_login/myhomepage.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,47 +12,56 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 204, 221, 226),
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 120,
-                width: 120,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: AssetImage('lib/images/beetlejuice.jpg')),
-                ),
-              ),
-              const Text("Beetle Juice",
-                  style: TextStyle(fontSize: 20, 
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 111, 88, 75)))
-            ],
-          ),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        title: const Text("Profile"), //Will display "User's Profile"
+        backgroundColor: const Color.fromARGB(255, 147, 163, 188),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage()),
+            );
+          },
         ),
-        const Divider(thickness: 2),
-        const Text("Bio:",
-                style: TextStyle(
-                fontWeight: FontWeight.bold, 
-                color: Color.fromARGB(255, 111, 88, 75)
-
-        ),),
-        const Divider(thickness: 2),
-        const TextSection(bio: "My favorite part of a retwist is being able to feel my scalp.")
-      ],
-    ),
-    ),
-    bottomNavigationBar: MyNavBar(),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CircleAvatar(
+                    radius: 70, 
+                    backgroundImage: AssetImage('lib/images/beetlejuice.jpg'),),
+                    Text("Beetle Juice",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 111, 88, 75)))
+                ],
+              ),
+            ),
+             Divider(thickness: 2),
+             Text(
+              "Bio:",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 111, 88, 75)),
+            ),
+             Divider(thickness: 2),
+             TextSection(
+                bio:
+                    "My favorite part of a retwist is being able to feel my scalp.")
+          ],
+        ),
+      ),
+      bottomNavigationBar: const MyNavBar(),
     );
   }
 }
