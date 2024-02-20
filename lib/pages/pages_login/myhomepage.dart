cc2b4f40f64914a8cc07d53736ca8d987c722215
Devon Pedraza <Devonpedraza123@gmail.com>
2024-02-20 17:42:48 -0500

    added begining of homepage

--- lib/pages/pages_login/myhomepage.dart
+++ lib/pages/pages_login/myhomepage.dart
@@ -7,7 +7,8 @@ import 'package:relay/pages/chatpage.dart';
 import 'package:relay/pages/profile.dart';
 import 'package:relay/pages/settings.dart';
 
-class MyHomePage extends StatefulWidget { // changed this to stateful widget
+class MyHomePage extends StatefulWidget {
+  // changed this to stateful widget
   const MyHomePage({super.key});
 
   @override
@@ -25,18 +26,82 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // padding constants
+    final double horizontalPadding = 5;
+    final double verticalPadding = 5;
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: signUserOut,
-             icon: const Icon(Icons.logout),
-             )
-         ],
-        ),
-      body: Center(child: Text(
-        "LOGGED IN AS: ${user.email!}",
-        style: const TextStyle(fontSize: 20),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // app bar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+                vertical: verticalPadding,
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'RELAY',
+                        style: TextStyle(fontSize: 32),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            //divider to separate the welcome back message
+            //and feed
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Divider(
+                thickness: 1,
+                color: Color.fromARGB(255, 204, 204, 204),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            // welcome back
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome Back,",
+                    style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
+                  ),
+                  const Text(
+                    'Devon Pedraza',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Search Bar
+            TextField(
+              style: TextStyle(color: Colors.black87),
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                  prefixIconColor: Colors.black87),
+            ),
+          ],
         ),
       ),
 
