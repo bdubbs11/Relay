b84aea3cf6a1a0c6f8297ad4cb914325a442db12
Brandon Wilson <brandonwilson@MacBook-Pro-193.local>
2024-02-27 21:16:57 -0500

    circular seach bar

--- lib/pages/pages_login/myhomepage.dart
+++ lib/pages/pages_login/myhomepage.dart
@@ -47,74 +47,87 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // app bar
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding,
-                vertical: verticalPadding,
-              ),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'RELAY',
-                        style: TextStyle(fontSize: 32),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            // Padding(
+            //   padding: EdgeInsets.symmetric(
+            //     horizontal: horizontalPadding,
+            //     vertical: verticalPadding,
+            //   ),
+            //   child: const Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: [
+            //       Row(
+            //         children: [
+            //           Text(
+            //             'RELAY',
+            //             style: TextStyle(fontSize: 32),
+            //           ),
+            //         ],
+            //       ),
+            //     ],
+            //   ),
+            // ),
 
-            const SizedBox(height: 10),
+           // const SizedBox(height: 10),
 
             //divider to separate the welcome back message
             //and feed
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0),
-              child: Divider(
-                thickness: 1,
-                color: Color.fromARGB(255, 204, 204, 204),
-              ),
-            ),
+            // const Padding(
+            //   padding: EdgeInsets.symmetric(horizontal: 40.0),
+            //   child: Divider(
+            //     thickness: 1,
+            //     color: Color.fromARGB(255, 204, 204, 204),
+            //   ),
+            // ),
 
-            const SizedBox(height: 10),
-            // welcome back
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Welcome Back,",
-                    style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
-                  ),
-                  const Text(
-                    'Devon Pedraza',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-            ),
+            // const SizedBox(height: 20),
+            // // welcome back
+            // Padding(
+            //   padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       Text(
+            //         "Welcome Back,",
+            //         style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
+            //       ),
+            //       const Text(
+            //         'Devon Pedraza',
+            //         style: TextStyle(fontSize: 20),
+            //       ),
+            //     ],
+            //   ),
+            // ),
 
-            const SizedBox(height: 25),
+            const SizedBox(height: 0),
 
             // Search Bar
-            TextField(
-              style: TextStyle(color: Colors.black87),
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  style: TextStyle(color: Colors.black87),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(color: AppColors.cloudBlue),
+                    ),
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                    prefixIconColor: Colors.black87,
                   ),
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                  prefixIconColor: Colors.black87),
+                ),
+              ),
             ),
+            // first  chat square
+
+
+
           ],
         ),
       ),
+
+
 
       // navbar stuff
       bottomNavigationBar: MyNavBar(currentIndex: (0)),
